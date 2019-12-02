5d271c2e1f3bd1781684e8b25a976bb49e720d77
Kudlaty <kfuks2@gmail.com>
2019-12-02 23:55:02 +0100

    fix some titles in schemas

--- src/mock_types.py
+++ src/mock_types.py
@@ -9,15 +9,16 @@ QueryStrings = Dict[str, List[str]]
 class HttpRequest(BaseModel):
     method: str
     path: str
-    query_string_parameters: QueryStrings = Schema(None, alias='queryStringParameters')
+    query_string_parameters: QueryStrings = Schema(None, title='queryStringParameters', alias='queryStringParameters')
 
 
 class HttpResponse(BaseModel):
-    status_code: int = Schema(200, alias='statusCode')
+    status_code: int = Schema(200, title='statusCode', alias='statusCode')
     headers: Dict[str, str] = None
     body: Any = Schema('', description='Body that will be returned')
     remaining_times: int = Schema(
         -1,
+        title='remainingTimes',
         alias='remainingTimes',
         description='Number of times this mock will be returned until deleted. -1 means unlimited'
     )
