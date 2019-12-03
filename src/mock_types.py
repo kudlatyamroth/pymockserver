bf8a7d6b021a1b225d9a2379efad5dc29c4398c1
Kudlaty <kfuks2@gmail.com>
2019-12-03 21:51:05 +0100

    remove disables of type checking, after update fastapi

--- src/mock_types.py
+++ src/mock_types.py
@@ -7,9 +7,9 @@ QueryStrings = Dict[str, List[str]]
 
 
 class HttpRequest(BaseModel):
-    method: str = Schema("GET", description="Http method", example="GET")  # type: ignore
-    path: str = Schema(..., description="Url path", example="/users")  # type: ignore
-    query_string_parameters: QueryStrings = Schema(  # type: ignore
+    method: str = Schema("GET", description="Http method", example="GET")
+    path: str = Schema(..., description="Url path", example="/users")
+    query_string_parameters: QueryStrings = Schema(
         None,
         title="queryStringParameters",
         alias="queryStringParameters",
@@ -19,21 +19,21 @@ class HttpRequest(BaseModel):
 
 
 class HttpResponse(BaseModel):
-    status_code: int = Schema(  # type: ignore
+    status_code: int = Schema(
         200, title="statusCode", alias="statusCode", description="Status code of mocked response", example=200,
     )
-    headers: Dict[str, str] = Schema(  # type: ignore
+    headers: Dict[str, str] = Schema(
         None, description="Headers included in mock response", example={"x-user": "John Doe",},
     )
-    body: Any = Schema("", description="Body that will be returned", example='"{"users":["John","Dave"]}"')  # type: ignore
-    remaining_times: int = Schema(  # type: ignore
+    body: Any = Schema("", description="Body that will be returned", example='"{"users":["John","Dave"]}"')
+    remaining_times: int = Schema(
         -1,
         title="remainingTimes",
         alias="remainingTimes",
         description="Number of times this mock will be returned until deleted. -1 means unlimited",
         example="-1",
     )
-    delay: int = Schema(0, description="How much milliseconds wait until response", example="500")  # type: ignore
+    delay: int = Schema(0, description="How much milliseconds wait until response", example="500")
 
 
 class CreatePayload(BaseModel):
