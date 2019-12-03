11e60431465024909394a7bb5b42f4834ed83811
Kudlaty <kfuks2@gmail.com>
2019-12-03 17:34:54 +0100

    more descriptive parameters in openapi

--- src/mock_types.py
+++ src/mock_types.py
@@ -7,22 +7,44 @@ QueryStrings = Dict[str, List[str]]
 
 
 class HttpRequest(BaseModel):
-    method: str
-    path: str
-    query_string_parameters: QueryStrings = Schema(None, title='queryStringParameters', alias='queryStringParameters')
+    method: str = Schema('GET', description='Http method', example='GET')
+    path: str = Schema(..., description='Url path', example='/users')
+    query_string_parameters: QueryStrings = Schema(
+        None,
+        title='queryStringParameters',
+        alias='queryStringParameters',
+        description='Query string parameters',
+        example={
+            'name': ['John'],
+            'age': ['25', '30'],
+        }
+    )
 
 
 class HttpResponse(BaseModel):
-    status_code: int = Schema(200, title='statusCode', alias='statusCode')
-    headers: Dict[str, str] = None
-    body: Any = Schema('', description='Body that will be returned')
+    status_code: int = Schema(
+        200,
+        title='statusCode',
+        alias='statusCode',
+        description='Status code of mocked response',
+        example=200,
+    )
+    headers: Dict[str, str] = Schema(
+        None,
+        description='Headers included in mock response',
+        example={
+            'x-user': 'John Doe',
+        },
+    )
+    body: Any = Schema('', description='Body that will be returned', example='"{\"users\":[\"John\",\"Dave\"]}"')
     remaining_times: int = Schema(
         -1,
         title='remainingTimes',
         alias='remainingTimes',
-        description='Number of times this mock will be returned until deleted. -1 means unlimited'
+        description='Number of times this mock will be returned until deleted. -1 means unlimited',
+        example='-1',
     )
-    delay: int = Schema(0, description='How much milliseconds wait until response')
+    delay: int = Schema(0, description='How much milliseconds wait until response', example='500')
 
 
 class CreatePayload(BaseModel):
