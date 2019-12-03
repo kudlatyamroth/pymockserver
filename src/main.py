fdf9143a6caa3379ebb2211073b13147f3cc98e1
Kudlaty <kfuks2@gmail.com>
2019-12-03 16:46:03 +0100

    move routes to separate file

--- src/main.py
+++ src/main.py
@@ -1,93 +1,15 @@
-import json
-import logging
-import time
-from typing import Dict
-
 import uvicorn
 from fastapi import FastAPI
-from starlette.requests import Request
-from starlette.responses import Response
-from starlette.status import HTTP_404_NOT_FOUND, HTTP_201_CREATED, HTTP_200_OK
 
-from mock_types import HttpRequest, CreatePayload, MockedData
-from utils import request_hash, query_params_to_http_qs
+import router
 
-app = FastAPI()
-logger = logging.getLogger("fastapi")
+app = FastAPI(
+    title="MockServer API",
+    description="Simple and fast mock server implemented in python",
+    version="0.5.1",
+)
 
-mocks: Dict[str, MockedData] = {}
-
-
-@app.post('/mockserver', status_code=HTTP_201_CREATED)
-async def add_mock(body: CreatePayload):
-    req_hash = request_hash(body.httpRequest)
-    mock = mocks.get(req_hash)
-    if mock:
-        mock.httpResponse.append(body.httpResponse)
-    else:
-        mocks[req_hash] = MockedData(httpRequest=body.httpRequest, httpResponse=[body.httpResponse])
-    logger.info(f'[MockServer] Added new mock for: {req_hash}')
-    return {'status': 'ok'}
-
-
-@app.get('/mockserver', status_code=HTTP_200_OK)
-async def get_mocks():
-    return mocks
-
-
-@app.delete('/mockserver', status_code=HTTP_200_OK)
-async def delete_routes(http_request: HttpRequest):
-    req_hash = request_hash(http_request)
-    logger.info(f'[MockServer] Deleted mock for: {req_hash}')
-    return {'removed': mocks.pop(req_hash, None), 'mocked': mocks}
-
-
-@app.delete('/mockserver/reset', status_code=HTTP_200_OK)
-async def clear_mocks():
-    mocks.clear()
-    logger.info('[MockServer] Clear all mocks')
-    return {'status': 'ok'}
-
-
-@app.post('{url_path:path}', include_in_schema=False)
-@app.patch('{url_path:path}', include_in_schema=False)
-@app.get('{url_path:path}', include_in_schema=False)
-@app.put('{url_path:path}', include_in_schema=False)
-@app.delete('{url_path:path}', include_in_schema=False)
-async def get_mocks(*, url_path: str = None, request: Request, response: Response):
-    http_request = HttpRequest(
-        method=request.method,
-        path=url_path,
-        queryStringParameters=query_params_to_http_qs(request.query_params.multi_items()),
-    )
-
-    req_hash = request_hash(http_request)
-    mock_list = mocks.get(req_hash)
-    if mock_list is None:
-        response.status_code = HTTP_404_NOT_FOUND
-        return {'code': HTTP_404_NOT_FOUND, 'status': 'Not found'}
-
-    mock = mock_list.httpResponse[0]
-    if 1 >= mock.remaining_times > -1:
-        if len(mock_list.httpResponse) > 1:
-            del mock_list.httpResponse[0]
-        else:
-            del mocks[req_hash]
-    if mock.remaining_times > 1:
-        mock.remaining_times -= 1
-
-    if mock.headers:
-        for header, value in mock.headers.items():
-            response.headers[header] = value
-
-    if mock.delay:
-        time.sleep(mock.delay/1000)
-
-    response.status_code = mock.status_code
-    try:
-        return json.loads(mock.body)
-    except (json.JSONDecodeError, TypeError):
-        return mock.body
+app.include_router(router.router)
 
 
 if __name__ == '__main__':
---
+++ src/router.py
@@ -0,0 +1,87 @@
+import json
+import time
+from typing import Dict
+
+from fastapi import APIRouter, HTTPException
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_404_NOT_FOUND
+
+from logger import logger
+from mock_types import CreatePayload, MockedData, HttpRequest
+from utils import request_hash, query_params_to_http_qs
+
+
+router = APIRouter()
+mocks: Dict[str, MockedData] = {}
+
+
+@router.post('/mockserver', status_code=HTTP_201_CREATED)
+async def add_mock(body: CreatePayload):
+    req_hash = request_hash(body.httpRequest)
+    mock = mocks.get(req_hash)
+    if mock:
+        mock.httpResponse.append(body.httpResponse)
+    else:
+        mocks[req_hash] = MockedData(httpRequest=body.httpRequest, httpResponse=[body.httpResponse])
+    logger.info(f'[MockServer] Added new mock for: {req_hash}')
+    return {'status': 'ok'}
+
+
+@router.get('/mockserver', status_code=HTTP_200_OK)
+async def get_mocks():
+    return mocks
+
+
+@router.delete('/mockserver', status_code=HTTP_200_OK)
+async def delete_routes(http_request: HttpRequest):
+    req_hash = request_hash(http_request)
+    logger.info(f'[MockServer] Deleted mock for: {req_hash}')
+    return {'removed': mocks.pop(req_hash, None), 'mocked': mocks}
+
+
+@router.delete('/mockserver/reset', status_code=HTTP_200_OK)
+async def clear_mocks():
+    mocks.clear()
+    logger.info('[MockServer] Clear all mocks')
+    return {'status': 'ok'}
+
+
+@router.post('{url_path:path}', include_in_schema=False)
+@router.patch('{url_path:path}', include_in_schema=False)
+@router.get('{url_path:path}', include_in_schema=False)
+@router.put('{url_path:path}', include_in_schema=False)
+@router.delete('{url_path:path}', include_in_schema=False)
+async def mock_response(*, url_path: str = None, request: Request, response: Response):
+    http_request = HttpRequest(
+        method=request.method,
+        path=url_path,
+        queryStringParameters=query_params_to_http_qs(request.query_params.multi_items()),
+    )
+
+    req_hash = request_hash(http_request)
+    mock_list = mocks.get(req_hash)
+    if mock_list is None:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail='Not found')
+
+    mock = mock_list.httpResponse[0]
+    if 1 >= mock.remaining_times > -1:
+        if len(mock_list.httpResponse) > 1:
+            del mock_list.httpResponse[0]
+        else:
+            del mocks[req_hash]
+    if mock.remaining_times > 1:
+        mock.remaining_times -= 1
+
+    if mock.headers:
+        for header, value in mock.headers.items():
+            response.headers[header] = value
+
+    if mock.delay:
+        time.sleep(mock.delay/1000)
+
+    response.status_code = mock.status_code
+    try:
+        return json.loads(mock.body)
+    except (json.JSONDecodeError, TypeError):
+        return mock.body
