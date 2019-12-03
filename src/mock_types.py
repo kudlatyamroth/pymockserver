cbd718a7ed64adc0b32e0f7ec50d2f11aa521228
Kudlaty <kfuks2@gmail.com>
2019-12-03 21:59:19 +0100

    fixes after updating pydantic

--- src/mock_types.py
+++ src/mock_types.py
@@ -1,15 +1,15 @@
 from typing import List, Dict, Any
 
-from pydantic import BaseModel, Schema
+from pydantic import BaseModel, Field
 
 
 QueryStrings = Dict[str, List[str]]
 
 
 class HttpRequest(BaseModel):
-    method: str = Schema("GET", description="Http method", example="GET")
-    path: str = Schema(..., description="Url path", example="/users")
-    query_string_parameters: QueryStrings = Schema(
+    method: str = Field("GET", description="Http method", example="GET")
+    path: str = Field(..., description="Url path", example="/users")
+    query_string_parameters: QueryStrings = Field(
         None,
         title="queryStringParameters",
         alias="queryStringParameters",
@@ -19,21 +19,21 @@ class HttpRequest(BaseModel):
 
 
 class HttpResponse(BaseModel):
-    status_code: int = Schema(
+    status_code: int = Field(
         200, title="statusCode", alias="statusCode", description="Status code of mocked response", example=200,
     )
-    headers: Dict[str, str] = Schema(
+    headers: Dict[str, str] = Field(
         None, description="Headers included in mock response", example={"x-user": "John Doe",},
     )
-    body: Any = Schema("", description="Body that will be returned", example='"{"users":["John","Dave"]}"')
-    remaining_times: int = Schema(
+    body: Any = Field("", description="Body that will be returned", example='"{"users":["John","Dave"]}"')
+    remaining_times: int = Field(
         -1,
         title="remainingTimes",
         alias="remainingTimes",
         description="Number of times this mock will be returned until deleted. -1 means unlimited",
         example="-1",
     )
-    delay: int = Schema(0, description="How much milliseconds wait until response", example="500")
+    delay: int = Field(0, description="How much milliseconds wait until response", example="500")
 
 
 class CreatePayload(BaseModel):
