30b5cdaf18ae7f0f0a911a2538f469424b635504
Kudlaty <kfuks2@gmail.com>
2020-01-16 09:14:48 +0100

    add more test for deleting mocks

--- tests/mockserver/test_delete_mock.py
+++ tests/mockserver/test_delete_mock.py
@@ -22,6 +22,77 @@ def test_should_delete_only_one_mock(client, create_mock):
 
 
 @pytest.mark.usefixtures("cleanup")
+def test_should_delete_only_mock_without_qs(client, create_mock):
+    path = "/users"
+    query_params = {"name": ["John"]}
+
+    create_mock(
+        {"httpRequest": {"path": path, "queryStringParameters": query_params,},}
+    )
+    mock = create_mock({"httpRequest": {"path": path,},})
+
+    del_response = client.delete("/mockserver", json=mock.get("data").get("httpRequest"))
+    assert del_response.status_code == 200
+
+    mock_response = client.get(path, params=query_params)
+    assert mock_response.status_code == 200
+
+    mock_response = client.get(path)
+    assert mock_response.status_code == 404
+
+
+@pytest.mark.usefixtures("cleanup")
+def test_should_delete_only_mock_with_qs(client, create_mock):
+    path = "/users"
+    query_params = {"name": ["John"]}
+
+    mock = create_mock({"httpRequest": {"path": path, "queryStringParameters": query_params,},})
+    create_mock(
+        {"httpRequest": {"path": path,},}
+    )
+
+    del_response = client.delete("/mockserver", json=mock.get("data").get("httpRequest"))
+    assert del_response.status_code == 200
+
+    mock_response = client.get(path)
+    assert mock_response.status_code == 200
+
+    mock_response = client.get(path, params=query_params)
+    assert mock_response.status_code == 404
+
+
+@pytest.mark.usefixtures("cleanup")
+def test_should_delete_only_mock_with_get_method(client, create_mock):
+    path = "/users"
+
+    create_mock(
+        {"httpRequest": {"path": path, "method": "POST",},}
+    )
+    create_mock(
+        {"httpRequest": {"path": path, "method": "PATCH",},}
+    )
+    create_mock(
+        {"httpRequest": {"path": path, "method": "PUT",},}
+    )
+    mock = create_mock({"httpRequest": {"path": path,},})
+
+    del_response = client.delete("/mockserver", json=mock.get("data").get("httpRequest"))
+    assert del_response.status_code == 200
+
+    mock_response = client.post(path)
+    assert mock_response.status_code == 200
+
+    mock_response = client.patch(path)
+    assert mock_response.status_code == 200
+
+    mock_response = client.put(path)
+    assert mock_response.status_code == 200
+
+    mock_response = client.get(path)
+    assert mock_response.status_code == 404
+
+
+@pytest.mark.usefixtures("cleanup")
 def test_should_not_delete_anything(client, create_mock):
     first_path = "/users"
     second_path = "/users2"
