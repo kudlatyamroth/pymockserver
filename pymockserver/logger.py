771d30f901356085b9c48c0483ee7cbd766bca6b
Kudlaty <kfuks2@gmail.com>
2020-01-17 19:26:50 +0100

    setup correct logger

--- pymockserver/logger.py
+++ pymockserver/logger.py
@@ -1,4 +1,13 @@
 import logging
+import sys
 
+logger = logging.getLogger("MockServer")
+logger.setLevel(logging.INFO)
 
-logger = logging.getLogger("fastapi")
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter("[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s")
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
