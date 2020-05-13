5633ea63b3ff4d9cd8742897b59a025986d672e5
Kudlaty <kfuks2@gmail.com>
2020-05-13 23:33:28 +0200

    fix(logger): remove duplicated app name in logs

--- pymockserver/managers.py
+++ pymockserver/managers.py
@@ -10,9 +10,9 @@ mocks: Dict[str, MockedData] = {}
 def get_mock(req_hash: str) -> Optional[MockedData]:
     mock = db.get(req_hash)
     if mock is None:
-        logger.info(f"[MockServer] Mock not found: {req_hash}")
+        logger.info(f"Mock not found: {req_hash}")
         return mock
-    logger.info(f"[MockServer] Found mock for hash: {req_hash}")
+    logger.info(f"Found mock for hash: {req_hash}")
     return mock
 
 
@@ -23,12 +23,12 @@ def decrease_remaining_times(mock: MockedData, req_hash: str) -> HttpResponse:
     if response.remaining_times > 1:
         mock.httpResponse[0].remaining_times -= 1
         db.set(req_hash, mock)
-        logger.info(f"[MockServer] Decreased remaining times in first mocked response for hash: {req_hash}")
+        logger.info(f"Decreased remaining times in first mocked response for hash: {req_hash}")
         return response
     if len(mock.httpResponse) > 1:
         del mock.httpResponse[0]
         db.set(req_hash, mock)
-        logger.info(f"[MockServer] Deleted first mocked response for hash: {req_hash}")
+        logger.info(f"Deleted first mocked response for hash: {req_hash}")
         return response
     delete_mock(req_hash)
     return response
@@ -38,12 +38,12 @@ def add_mock(req_hash: str, payload: CreatePayload) -> MockedData:
     mock = db.get(req_hash)
     if mock:
         mock.httpResponse.append(payload.httpResponse)
-        logger.info(f"[MockServer] Append mock to hash: {req_hash}")
+        logger.info(f"Append mock to hash: {req_hash}")
         db.set(req_hash, mock)
         return mock
     mock = MockedData(httpRequest=payload.httpRequest, httpResponse=[payload.httpResponse])
     db.set(req_hash, mock)
-    logger.info(f"[MockServer] Added new mock for: {req_hash}")
+    logger.info(f"Added new mock for: {req_hash}")
     return mock
 
 
@@ -52,10 +52,10 @@ def get_mocks() -> Dict:
 
 
 def delete_mock(req_hash: str) -> Optional[MockedData]:
-    logger.info(f"[MockServer] Deleting mocks for hash: {req_hash}")
+    logger.info(f"Deleting mocks for hash: {req_hash}")
     return db.delete(req_hash)
 
 
 def purge_mocks() -> None:
     db.clear()
-    logger.info("[MockServer] Clear all mocks")
+    logger.info("Clear all mocks")
