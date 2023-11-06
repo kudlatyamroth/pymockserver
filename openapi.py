282e5113a157107d4e2ca02100d45750f5eba4dd
Kudlaty <kfuks2@gmail.com>
2023-11-06 12:29:02 +0100

    feat: remove useless scripts

---
+++ openapi.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import json
+import sys
+from pathlib import Path
+
+
+project_dir = Path(__file__).parent
+
+
+def generate_openapi():
+    from main import app
+
+    with open(str(project_dir.joinpath("openapi.json")), "w") as openapi_file:
+        json.dump(app.openapi(), openapi_file, indent=2)
+
+
+print("Save openApi file")
+
+sys.path.append(str(project_dir.joinpath("pymockserver")))
+
+generate_openapi()
+
+print("Generated new openapi")
